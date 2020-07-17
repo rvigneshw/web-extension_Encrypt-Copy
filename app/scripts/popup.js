a40030863dc40af76212d23f69caa997f129b830
Vigneshwaran R <vigneshw1811@gmail.com>
2020-07-17 16:40:19 +0530

    New Icon based actions

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,29 +1,37 @@
 import { AES, enc } from "crypto-js";
-function attachEventListenerTo(className, eventHandler) {
-    var allIcons = document.getElementsByClassName(className);
-    for (let i = 0; i < allIcons.length; i++) {
-        allIcons[i].addEventListener('click', eventHandler);
+const DEBUG = true;
+function attachEventListenerTo(className, event, eventHandler) {
+    var allElements = document.getElementsByClassName(className);
+    for (let i = 0; i < allElements.length; i++) {
+        allElements[i].addEventListener(event, eventHandler);
     }
 }
-
+function changeToTick(img) {
+    img.src = "../images/check.svg"
+}
 function handleDVclick(event) {
     var encData = event.srcElement.dataset.data;
     decryptAndView(encData, event);
+    changeToTick(event.srcElement);
 }
 function handleCCEclick(event) {
     var encData = event.srcElement.dataset.data;
     copyToClipboard(encData);
+    changeToTick(event.srcElement);
+    d(event.srcElement);
 }
 function handleCCDclick(event) {
     var encData = event.srcElement.dataset.data;
     decryptAndCopyToClipboard(encData);
+    changeToTick(event.srcElement);
 }
 function handleDELclick(event) {
     var timeStampData = event.srcElement.dataset.timestamp;
     deleteItemFromStorage(timeStampData);
+    changeToTick(event.srcElement);
 }
 function handleSettingsChange(event) {
-    var settings_history = document.getElementById("settings_history");
+    var settings_history = getByID("settings_history");
     var updated_settings = {
         history: settings_history.checked,
         decrypt: settings_view_decrypt.checked
@@ -31,6 +39,7 @@ function handleSettingsChange(event) {
     browser.storage.local.set({
         settings: updated_settings
     });
+    initialize();
 }
 function handleClearReset() {
     browser.runtime.sendMessage({
@@ -46,39 +55,45 @@ function handleViewIntroPage() {
 const EVENT_LISTENER_ARRAY = [
     {
         "class": "d_v_btn",
+        "event": "click",
         "handler": handleDVclick
     },
     {
         "class": "cc_e_btn",
+        "event": "click",
         "handler": handleCCEclick
     },
     {
         "class": "cc_d_btn",
+        "event": "click",
         "handler": handleCCDclick
     },
     {
         "class": "del_btn",
+        "event": "click",
         "handler": handleDELclick
     },
     {
         "class": "clear_reset_btn",
+        "event": "click",
         "handler": handleClearReset
     },
     {
         "class": "view_intro_btn",
+        "event": "click",
         "handler": handleViewIntroPage
     },
     {
         "class": "settings-control",
+        "event": "click",
         "handler": handleSettingsChange
     }
 ]
 
 function updateVersionTextAndGreetings() {
     var version = browser.runtime.getManifest().version;
-    document.getElementById("wand-tools-version-text")
+    getByID("wand-tools-version-text")
         .innerHTML = `Version : ${version}`;
-
 
     var greetdate = new Date()
     var hours = greetdate.getHours()
@@ -93,8 +108,7 @@ function updateVersionTextAndGreetings() {
     } else if (hours < 24) {
         greetingText = 'Good Night!';
     }
-
-    document.getElementById("greetings").innerHTML = greetingText;
+    getByID("greetings").innerHTML = greetingText;
 }
 
 
@@ -102,70 +116,51 @@ initialize();
 
 function initialize() {
     browser.storage.local.get().then((data) => {
-        console.log(data)
+        d(data);
         updateVersionTextAndGreetings();
         var listItems = '';
         if (data.copied_items) {
             var local_copied_items = data.copied_items;
-            document.getElementById("settings_history").checked = data.settings.history;
-            document.getElementById("settings_view_decrypt").checked = data.settings.decrypt;
-            document.getElementById("num_of_copied_items").innerText = local_copied_items.length;
+            getByID("settings_history").checked = data.settings.history;
+            getByID("num_of_copied_items").innerText = local_copied_items.length;
             local_copied_items.forEach(element => {
-                if (data.settings.decrypt) {
-                    listItems = generateCards(
-                        listItems,
-                        element.data,
-                        element.time,
-                        decryptText(element.data, data.secret_phrase),
-                        data.settings.decrypt);
-                } else {
-                    listItems = generateCards(
-                        listItems,
-                        element.data,
-                        element.time,
-                        "",
-                        data.settings.decrypt);
-                }
+
+                listItems = generateCards(
+                    listItems,
+                    element.data,
+                    element.time,
+                    "",
+                    data.settings.decrypt);
+
             });
-            console.log(listItems)
-            document.getElementById("history_items")
+            getByID("history_items")
                 .innerHTML = listItems;
             EVENT_LISTENER_ARRAY.forEach(element => {
-                attachEventListenerTo(element.class, element.handler);
+                attachEventListenerTo(element.class, element.event, element.handler);
             });
         }
     });
 
-    function generateCards(listItems, data, time, decData, decrypt) {
+    function generateCards(listItems, data, time) {
         listItems += `<li>
             <div>
-        <div class="uk-card uk-card-hover uk-card-default uk-card-small uk-card-body">
+        <div class=" uk-card uk-card-hover uk-card-default uk-card-small uk-card-body" >
             <div class="uk-card-badge uk-label">${time}</div>
             <span class="uk-text-right uk-text-emphasis uk-text-primary"> 
                 
-                <div class="uk-inline">
-                    <button  class="uk-button uk-button-small uk-button-secondary" type="button">More Options</button>
-                    <div uk-dropdown="mode: click; boundary: ! .uk-button-group; boundary-align: true;">
-                        <ul class="uk-nav uk-dropdown-nav">
-                        <li><button data-timestamp="${time}" class="del_btn uk-button uk-button-small uk-button-danger uk-width-1-1 uk-margin-small-bottom">Delete</button></li>`
-            +
-            (
-                (decrypt) ?
-                    `` :
-                    `<li><button data-data="${data}" class="d_v_btn uk-button uk-button-small uk-button-primary uk-width-1-1 uk-margin-small-bottom" type="button">Decrypt and View</button></li>`
-            )
-            +
-            `<li><button data-data="${data}" class="cc_e_btn uk-button uk-button-small uk-button-primary uk-width-1-1 uk-margin-small-bottom" type="button">
-                            Copy to Clipboard (Encrypted Text)
-                            </button></li>
-                            <li><button data-data="${data}" class="cc_d_btn uk-button uk-button-small uk-button-primary uk-width-1-1 uk-margin-small-bottom" type="button">
-                            Copy to Clipboard (Decrypted Text)
-                            </button></li>
-                        </ul>
-                    </div>
+                <div class="uk-inline icon-array">
+                    
+                    <img uk-tooltip="Delete this" class="icons-array-icons del_btn" src="../images/trash.svg" height="25px" width="25px" data-timestamp="${time}">
+                    
+                    <img uk-tooltip="Copy the decrypted data to clipboard" class="icons-array-icons cc_d_btn" src="../images/crypt-key.svg" height="25px" width="25px" data-data="${data}">
+                                        
+                    <img uk-tooltip="View the decrypted data" class="icons-array-icons d_v_btn" src="../images/visibility.svg" height="25px" width="25px" data-data="${data}">
+                    
+                    <img uk-tooltip="Copy the encrypted data to clipboard" class="icons-array-icons cc_e_btn" src="../images/copy.svg" height="25px" width="25px" data-data="${data}">
+
                 </div>
             </span>
-            <p class="uk-text-break">${(decrypt) ? decData : data}</p>
+            <p class="uk-text-break">${data}</p>
         </div>
     </div></li>`;
         return listItems;
@@ -186,18 +181,18 @@ function deleteItemFromStorage(timeStamp) {
 }
 
 function decryptAndView(data, event) {
-    console.log(data)
+
     browser.runtime.sendMessage({
         "type": "DAV",
         "content": data
     }).then(handleResponse);
     function handleResponse(message) {
-        console.log(message)
-        event.srcElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.lastElementChild.innerText = message.response
+
+        event.srcElement.parentElement.parentElement.parentElement.lastElementChild.innerText = message.response
     }
 }
 function decryptAndCopyToClipboard(data) {
-    console.log(data)
+
     browser.runtime.sendMessage({
         "type": "DAV",
         "content": data
@@ -220,13 +215,11 @@ function copyToClipboard(text) {
     });
 }
 
-function decryptText(text, token) {
-    try {
-        var bytes = AES.decrypt(JSON.stringify({ text }), JSON.stringify({ token }));
-        var originalText = bytes.toString(enc.Utf8);
-        console.log(originalText);
-        return originalText;
-    } catch (error) {
-        console.log(error);
-    }
+
+function getByID(id) {
+    return document.getElementById(id);
+}
+
+function d(data) {
+    if (DEBUG) console.log(data);
 }
